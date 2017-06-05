704adf9e80aab914b70f2112a6181fd51d2e9e29
MrZhaozhirong <2377871036@qq.com>
2017-06-05 15:22:09 +0800

    完善yuv显示，圆形模型改成索引画图

--- app/src/main/java/com/earth/opengl/shape/YUV_ELEMENT_Ball.java
+++ app/src/main/java/com/earth/opengl/shape/YUV_ELEMENT_Ball.java
@@ -4,15 +4,14 @@ import android.content.Context;
 import android.opengl.GLES20;
 import android.util.Log;
 
+import com.earth.opengl.data.IndexBuffer;
+import com.earth.opengl.data.VertexBuffer;
 import com.earth.opengl.utils.MatrixHelper;
 import com.earth.opengl.utils.ShaderHelper;
 import com.earth.opengl.utils.TextResourceReader;
 import com.earth.opengl.utils.TextureHelper;
 import com.pixel.opengl.R;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.FloatBuffer;
 import java.util.ArrayList;
 
 
@@ -42,15 +41,14 @@ public class YUV_ELEMENT_Ball {
     public double moving_count_auto_return = 0.0f;
     //*****************************************************************
     private final Context context;
-    final int angleSpan = 4;   // 将球行单位进切分的角度
-    private static final float UNIT_SIZE = 1.0f;    // 单位尺寸
-    private float radius = 0.8f;    // 球的半径
-    private int vCount = 0;     // 记录顶点个数，先初始化为0
-    private static final int BYTES_PER_FLOAT = 4;   // float类型的字节数
-    private static final int COORDS_PER_VERTEX = 3; // 数组中每个顶点的坐标数
-    private static final int TEXTURE_COORDIANTES_COMPONENT_COUNT = 2; // 每个纹理坐标为 S T两个
-    private FloatBuffer vertexBuffer;// 顶点坐标
-    private FloatBuffer textureBuffer;// 纹理坐标
+    private final int angleSpan = 5;// 将球行单位进切分的角度
+    private static final float UNIT_SIZE = 1.0f;
+    private float radius = 0.8f;
+    private int vCount = 0;     // 记录顶点个数，先初始化为0，一个顶点由xyz和st 5个float构成
+    private int numElements = 0;// 记录要画多少个三角形
+    private static final int BYTES_PER_FLOAT = 4;
+    private static final int POSITION_COORDIANTE_COMPONENT_COUNT = 3; // 每个顶点的坐标数 x y z
+    private static final int TEXTURE_COORDIANTE_COMPONENT_COUNT = 2; // 每个纹理坐标为 S T两个
     private int program;
     private static final String A_POSITION = "a_Position";
     private static final String U_MATRIX = "u_Matrix";
@@ -58,46 +56,42 @@ public class YUV_ELEMENT_Ball {
     private int aPositionLocation;
     private static final String A_TEXTURE_COORDINATES = "a_TextureCoordinates";
     private int aTextureCoordinates;
-
+    //**************************增加YUV支持*************************************
     private static final String SAMPLER_Y = "SamplerY";
     private int uLocationSamplerY;
     private static final String SAMPLER_U = "SamplerU";
     private int uLocationSamplerU;
     private static final String SAMPLER_V = "SamplerV";
     private int uLocationSamplerV;
-
+    //**************************坐标改成索引************************************
+    private static final int STRIDE = (POSITION_COORDIANTE_COMPONENT_COUNT
+            + TEXTURE_COORDIANTE_COMPONENT_COUNT)
+            * BYTES_PER_FLOAT;
+    private VertexBuffer vstBuffer;
+    private IndexBuffer indexBuffer;
 
     public YUV_ELEMENT_Ball(Context context){
         this.context = context;
-        initVertexData();
+
+        createIndexAndVSTData();
+
         buildProgram();
 
-        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
-        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
-        aTextureCoordinates = GLES20.glGetAttribLocation(program, A_TEXTURE_COORDINATES);
-        uLocationSamplerY = GLES20.glGetUniformLocation(program, SAMPLER_Y);
-        uLocationSamplerU = GLES20.glGetUniformLocation(program, SAMPLER_U);
-        uLocationSamplerV = GLES20.glGetUniformLocation(program, SAMPLER_V);
+        initShaderVariable();
 
         initTexture();
 
-        //---------传入顶点坐标数据
-        GLES20.glVertexAttribPointer(aPositionLocation, COORDS_PER_VERTEX,
-                GLES20.GL_FLOAT, false, 0, vertexBuffer);
-        GLES20.glEnableVertexAttribArray(aPositionLocation);
-        //---------传入纹理坐标数据
-        GLES20.glVertexAttribPointer(aTextureCoordinates, TEXTURE_COORDIANTES_COMPONENT_COUNT,
-                GLES20.GL_FLOAT, false, 0, textureBuffer);
-        GLES20.glEnableVertexAttribArray(aTextureCoordinates);
+        setAttributeStatus();
     }
 
-    private void initVertexData() {
-        ArrayList<Float> alVertix = new ArrayList<Float>();// 存放顶点坐标的ArrayList
-        // ***************************************
-        ArrayList<Float> textureVertix = new ArrayList<Float>();// 存放纹理坐标的ArrayList
-        for (int vAngle = 0; vAngle < 180; vAngle = vAngle + angleSpan) //球面垂直方向180/angleSpan数量单位
+
+    private void createIndexAndVSTData() {
+        ArrayList<Float> VSTList = new ArrayList<Float>();// V顶点坐标(xyz)，ST纹理坐标(st)
+        ArrayList<Short> indexList = new ArrayList<Short>();// 顶点所以数组
+        short offset = 0;
+        for (int vAngle = 0; vAngle < 180; vAngle = vAngle + angleSpan)
         {
-            for (int hAngle = 0; hAngle <= 360; hAngle = hAngle + angleSpan)//球面水平方向360/angleSpan数量单位
+            for (int hAngle = 0; hAngle <= 360; hAngle = hAngle + angleSpan)
             {
                 // 纵向横向各到一个角度后计算对应的此点在球面上的坐标
                 float z0 = (float) (radius * UNIT_SIZE
@@ -135,80 +129,78 @@ public class YUV_ELEMENT_Ball {
                         .sin(Math.toRadians(hAngle)));
                 float y3 = (float) (radius * UNIT_SIZE * Math.cos(Math
                         .toRadians(vAngle + angleSpan)));
-                //Log.w("x3 y3 z3","" + x3 + "  "+y3+ "  " +z3);
-                // 一个矩形由两个三角形组成
-                // 第一个三角形的三个点
-                alVertix.add(x1);
-                alVertix.add(y1);
-                alVertix.add(z1);
-                alVertix.add(x3);
-                alVertix.add(y3);
-                alVertix.add(z3);
-                alVertix.add(x0);
-                alVertix.add(y0);
-                alVertix.add(z0);
-                // 关于纹理坐标 Android Opengl的坐标 http://www.cnblogs.com/jenry/p/4083415.html
-                float s0 = hAngle / 360.0f;
-                float t0 = 1 - vAngle / 180.0f;
-                float s1 = (hAngle + angleSpan)/360.0f ;
-                float t1 = 1 - (vAngle + angleSpan) / 180.0f;
-                // Android设备y坐标是反向的，正常图显示到设备上是水平颠倒的。
-                t0 = 1 - t0;
-                t1 = 1 - t1;
 
-                textureVertix.add(s1);
-                textureVertix.add(t0);
-                textureVertix.add(s0);
-                textureVertix.add(t1);
-                textureVertix.add(s0);
-                textureVertix.add(t0);
-                // 第二个三角形的三个点
-                alVertix.add(x1);
-                alVertix.add(y1);
-                alVertix.add(z1);
-                alVertix.add(x2);
-                alVertix.add(y2);
-                alVertix.add(z2);
-                alVertix.add(x3);
-                alVertix.add(y3);
-                alVertix.add(z3);
-                // 第二个三角形对应的纹理坐标
-                textureVertix.add(s1);
-                textureVertix.add(t0);
-                textureVertix.add(s1);
-                textureVertix.add(t1);
-                textureVertix.add(s0);
-                textureVertix.add(t1);
+                float s0 = hAngle / 360.0f;
+                float t0 = vAngle / 180.0f;
+                float s1 = (hAngle + angleSpan)/360.0f ;
+                float t1 = (vAngle + angleSpan) / 180.0f;
+
+                //左下角
+                VSTList.add(x0);
+                VSTList.add(y0);
+                VSTList.add(z0);
+                VSTList.add(s0);
+                VSTList.add(t0);
+
+                //右下角
+                VSTList.add(x1);
+                VSTList.add(y1);
+                VSTList.add(z1);
+                VSTList.add(s1);
+                VSTList.add(t0);
+
+                //右上角
+                VSTList.add(x2);
+                VSTList.add(y2);
+                VSTList.add(z2);
+                VSTList.add(s1);
+                VSTList.add(t1);
+
+                //左上角
+                VSTList.add(x3);
+                VSTList.add(y3);
+                VSTList.add(z3);
+                VSTList.add(s0);
+                VSTList.add(t1);
+
+                indexList.add((short)(offset + 1));
+                indexList.add((short)(offset + 3));
+                indexList.add((short)(offset + 0));
+                indexList.add((short)(offset + 1));
+                indexList.add((short)(offset + 2));
+                indexList.add((short)(offset + 3));
+
+                offset += 4; // 4个顶点
+
+
             }
         }
         // *****************************************************************
-        vCount = alVertix.size() / COORDS_PER_VERTEX;// 顶点的数量
-        // 将alVertix中的坐标值转存到一个float数组中
-        float vertices[] = new float[ alVertix.size() ];
-        for (int i = 0; i < alVertix.size(); i++) {
-            vertices[i] = alVertix.get(i);
+        vCount = VSTList.size() / (POSITION_COORDIANTE_COMPONENT_COUNT + TEXTURE_COORDIANTE_COMPONENT_COUNT);
+        numElements = indexList.size();// 每4个顶点构成2个三角形元素 * 3个索引点
+
+        Log.w(TAG, "VSTList.size : "+VSTList.size()); //多少个float数据
+        Log.w(TAG, "indexList.size : "+indexList.size());
+
+        float[] element_VSTData = new float[VSTList.size()];
+        for (int i = 0; i < VSTList.size(); i++) {
+            element_VSTData[i] = VSTList.get(i);
         }
-        vertexBuffer = ByteBuffer
-                .allocateDirect(vertices.length * BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
-        // 把坐标们加入FloatBuffer中
-        vertexBuffer.put(vertices);
-        // 设置buffer，从第一个坐标开始读
-        vertexBuffer.position(0);
-        // *****************************************************************
-        float textures[] = new float[textureVertix.size()];
-        for(int i=0;i<textureVertix.size();i++){
-            textures[i] = textureVertix.get(i);
+        vstBuffer = new VertexBuffer(element_VSTData);
+        short[] element_index = new short[indexList.size()];
+        for (int i = 0; i < indexList.size(); i++) {
+            element_index[i] = indexList.get(i);
         }
-        textureBuffer = ByteBuffer
-                .allocateDirect(textures.length * BYTES_PER_FLOAT)
-                .order(ByteOrder.nativeOrder())
-                .asFloatBuffer();
-        // 把坐标们加入FloatBuffer中
-        textureBuffer.put(textures);
-        // 设置buffer，从第一个坐标开始读
-        textureBuffer.position(0);
+        indexBuffer = new IndexBuffer(element_index);
+    }
+
+    private void initShaderVariable() {
+        aPositionLocation = GLES20.glGetAttribLocation(program, A_POSITION);
+        uMatrixLocation = GLES20.glGetUniformLocation(program, U_MATRIX);
+        aTextureCoordinates = GLES20.glGetAttribLocation(program, A_TEXTURE_COORDINATES);
+        uLocationSamplerY = GLES20.glGetUniformLocation(program, SAMPLER_Y);
+        uLocationSamplerU = GLES20.glGetUniformLocation(program, SAMPLER_U);
+        uLocationSamplerV = GLES20.glGetUniformLocation(program, SAMPLER_V);
     }
 
     private void buildProgram() {
@@ -224,10 +216,6 @@ public class YUV_ELEMENT_Ball {
     }
 
     private boolean initTexture() {
-
-        GLES20.glUniform1i(uLocationSamplerY, 0); // => GLES20.GL_TEXTURE0
-        GLES20.glUniform1i(uLocationSamplerU, 1); // => GLES20.GL_TEXTURE1
-        GLES20.glUniform1i(uLocationSamplerV, 2); // => GLES20.GL_TEXTURE2
         int[] yuvTextureIDs = TextureHelper.loadYUVTexture(context, R.raw.yuv_test_pic);
         if(yuvTextureIDs == null || yuvTextureIDs.length != 3) {
             Log.w(TAG,"yuvTextureIDs object's length not equals 3 !");
@@ -235,21 +223,35 @@ public class YUV_ELEMENT_Ball {
         }
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, yuvTextureIDs[0]);
+        GLES20.glUniform1i(uLocationSamplerY, 0); // => GLES20.GL_TEXTURE0
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE1);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, yuvTextureIDs[1]);
+        GLES20.glUniform1i(uLocationSamplerU, 1); // => GLES20.GL_TEXTURE1
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE2);
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, yuvTextureIDs[2]);
-
+        GLES20.glUniform1i(uLocationSamplerV, 2); // => GLES20.GL_TEXTURE2
         return true;
+    }
+
+    public void setAttributeStatus() {
+        vstBuffer.setVertexAttribPointer(0,
+                aPositionLocation,
+                POSITION_COORDIANTE_COMPONENT_COUNT, STRIDE);
+        vstBuffer.setVertexAttribPointer(
+                POSITION_COORDIANTE_COMPONENT_COUNT * BYTES_PER_FLOAT,
+                aTextureCoordinates,
+                TEXTURE_COORDIANTE_COMPONENT_COUNT, STRIDE);
     }
 
     public void draw(){
         //将最终变换矩阵写入
         GLES20.glUniformMatrix4fv(uMatrixLocation, 1, false, MatrixHelper.getFinalMatrix(),0);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vCount);
-    }
 
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, indexBuffer.getIndexBufferId());
+        GLES20.glDrawElements(GLES20.GL_TRIANGLES, numElements, GLES20.GL_UNSIGNED_SHORT, 0);
+        GLES20.glBindBuffer(GLES20.GL_ELEMENT_ARRAY_BUFFER, 0);
+    }
 
 }
